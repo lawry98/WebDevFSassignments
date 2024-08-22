b69e72517a7cec8ce530950b4a43df04c02e903b
Lawrence Crasto <lawry1998@gmail.com>
2024-08-22 17:14:08 -0400

    week 3 assignements

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,19 +1,31 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectID,
+        ref: 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    description: String,
+    price: Number,
+    imagelink: String
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
--- week-3/04-mongo-with-jwt-auth/routes/admin.js
+++ week-3/04-mongo-with-jwt-auth/routes/admin.js
@@ -1,14 +1,30 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db");
 const router = Router();
-
+const jwt = require('jsonwebtoken');
+const jwtPassword = "secret123";
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async(req, res) => {
     // Implement admin signup logic
+    const user = req.body.username;
+    const pass = req.body.password;
+    await Admin.create({username:user, password:pass});
+    res.status(200).json({message: 'Admin created successfully'});
 });
 
-router.post('/signin', (req, res) => {
+router.post('/signin', async(req, res) => {
     // Implement admin signup logic
+    const user = req.body.username;
+    const pass = req.body.password;
+    Admin.findOne({username:user, password:pass}).then((value) => {
+        if(value){
+            const token = jwt.sign(user, jwtPassword);
+            res.status(200).json({ token: token });
+        } else {
+            res.sendStatus(404);
+        }
+    });
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
